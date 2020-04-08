074bc1c7664af196e4594c99f78e17a0ed92012a
Misael Cureño <curenosm@gmail.com>
2020-04-08 17:16:56 -0500

    New git alias

--- start.sh
+++ start.sh
@@ -2,8 +2,12 @@
 
 echo "alias ll='ls -al --group-directories-first --color'" >> ~/.bashrc
 echo "alias xc='clear'" >> ~/.bashrc
+echo "alias tr='tree -dL'" >> ~/.bashrc
 
 # Alias for fast use of Git
+echo "alias gst='git status'" >> ~/.bashrc
+echo "alias glo='git log'" >> ~/.bashrc
 echo "alias gaa='git add -A'" >> ~/.bashrc
 echo "alias gcm='git commit -m'" >> ~/.bashrc
+echo "alias gpuo='git pull origin'" >> ~/.bashrc
 echo "alias gpo='git push origin'" >> ~/.bashrc
