200d99ee7715846f7420e9fa1866d018acdedea1
Misael Cureño <curenosm@gmail.com>
2020-04-15 20:16:01 -0500

    Environment variables added to print text in colors

--- start.sh
+++ start.sh
@@ -14,4 +14,26 @@ echo "alias gpo='git push origin'" >> ~/.bashrc
 
 echo "alias whichservice='sudo netstat -ltnp | grep -w'" >> ~/.bashrc
 
-echo "alias dcxweb='docker-compose exec web'" >> ~/.bashrc 
+echo "alias dcxweb='docker-compose exec web'" >> ~/.bashrc
+
+# Colors
+echo "" >> ~/.bashrc
+echo "export BLACK='\033[0;30m'" >> ~/.bashrc
+echo "export DGRAY='\033[1;30m'" >> ~/.bashrc
+echo "export RED='\033[0;31m'" >> ~/.bashrc
+echo "export LRED='\033[1;31'" >> ~/.bashrc
+echo "export GREEN='\033[0;32m'" >> ~/.bashrc
+echo "export LGREEN='\033[1;32m'" >> ~/.bashrc
+echo "export ORANGE='\033[0;33m'" >> ~/.bashrc
+echo "export YELLOW='\033[1;33m'" >> ~/.bashrc
+echo "export BLUE='\033[0;34m'" >> ~/.bashrc
+echo "export LBLUE='\033[1;34m'" >> ~/.bashrc
+echo "export PURPLE='\033[0;35m'" >> ~/.bashrc
+echo "export LPURPLE='\033[1;35m'" >> ~/.bashrc
+echo "export CYAN='\033[0;36m'" >> ~/.bashrc
+echo "export LCYAN='\033[1;36m'" >> ~/.bashrc
+echo "export LGRAY='\033[0;37m'" >> ~/.bashrc
+echo "export WHITE='\033[1;37m'" >> ~/.bashrc
+echo "export NC='\033[0m'" >> ~/.bashrc
+echo "export RED='\033[0;31m'" >> ~/.bashrc
+
