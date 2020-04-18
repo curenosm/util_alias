3d364660b40ca05cd4a28685a1310fb3a993b188
Misael Cureño <curenosm@gmail.com>
2020-04-18 09:29:39 -0500

    Git alias for displaying branches graph

--- start.sh
+++ start.sh
@@ -37,3 +37,10 @@ echo "export WHITE='\033[1;37m'" >> ~/.bashrc
 echo "export NC='\033[0m'" >> ~/.bashrc
 echo "export RED='\033[0;31m'" >> ~/.bashrc
 
+
+################ Git graph ###############################
+
+echo "[alias]" >> ~/.gitconfig
+echo "lg1 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all" >> ~/.gitconfig
+echo "lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all" >> ~/.bashrc
+echo 'lg = !"git lg1"' >> ~/.gitconfig
